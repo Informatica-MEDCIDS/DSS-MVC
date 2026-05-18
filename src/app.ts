8c725b7a7774dcde5d1ce889607677e01c2106e9
Pedro Marques <pmarques@med.up.pt>
2026-05-18 23:17:06 +0100

    Sem controller

--- src/app.ts
+++ src/app.ts
@@ -5,29 +5,44 @@ import { Prescricao } from './models/prescricao.entity';
 import { Exame } from './models/exame.entity';
 import exameRoutes from './routes/exame.routes';
 import prescricaoRoutes from './routes/prescricao.routes';
+import fhirRoutes from './routes/fhir.routes';
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(express.json());
+
 app.use('/prescricoes', prescricaoRoutes);
 app.use('/exames', exameRoutes);
 app.use('/pedidos-exames', exameRoutes);
+app.use('/fhir', fhirRoutes);
 
 if (require.main === module) {
     AppDataSource.initialize().then(async () => {
 
         const prescricaoRepo = AppDataSource.getRepository(Prescricao);
         if (await prescricaoRepo.count() === 0) {
-            await prescricaoRepo.save({ medicamento: 'Aspirina', dose: '500mg', medico_nome: 'Dr. House', dataCriacao: new Date() });
+            await prescricaoRepo.save({
+                medicamento: 'Aspirina',
+                dose: '500mg',
+                medico_nome: 'Dr. House',
+                dataCriacao: new Date()
+            });
         }
 
         const exameRepo = AppDataSource.getRepository(Exame);
         if (await exameRepo.count() === 0) {
-            await exameRepo.save({ nome: 'RX Torax', codigo: 'RX01', medico_nome: 'Dr. House', dataCriacao: new Date() });
+            await exameRepo.save({
+                nome: 'RX Torax',
+                codigo: 'RX01',
+                medico_nome: 'Dr. House',
+                dataCriacao: new Date()
+            });
         }
 
-        app.listen(3000, () => console.log("Servidor (TypeORM + SQLite) a correr na porta 3000"));
+        app.listen(3000, () =>
+            console.log("Servidor (TypeORM + SQLite) a correr na porta 3000")
+        );
     });
 }
 
