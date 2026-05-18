959db846ba84aad7b3414c90f49bf568b6d9a3c1
Pedro Marques <pmarques@med.up.pt>
2026-05-18 20:47:10 +0100

    Extras

--- src/database/database.ts
+++ src/database/database.ts
@@ -3,6 +3,8 @@ import { DataSource } from 'typeorm';
 import { Prescricao } from '../models/prescricao.entity';
 import { Exame } from '../models/exame.entity';
 
+// Configuração do DataSource para o TypeORM usando better-sqlite3
+
 export const AppDataSource = new DataSource({
     type: 'better-sqlite3',
     database: 'data.db',
